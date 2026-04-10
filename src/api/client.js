2a128c3388f36f65af5c88860a50568e558a0e15
Anvitha-neena <23wh1a0553@bvrithyderabad.edu.in>
2026-04-10 18:12:46 +0530

    updated Question component file

--- src/api/client.js
+++ src/api/client.js
@@ -1,2 +1,34 @@
-// TODO: Set the base URL for your API
+const BASE_URL = 'http://bvritcloud.com/api';
+const STUDENT_ID = '23WH1A0553';
+
+const apiCall = (method, endpoint, data = null) => {
+  const options = {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      'x-student-id': STUDENT_ID,
+    },
+  };
+
+  if (data) {
+    options.body = JSON.stringify(data);
+  }
+
+  return fetch(`${BASE_URL}${endpoint}`, options)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`API error: ${res.status}`);
+      }
+      return res.json();
+    })
+    .catch(err => {
+      console.error('API call failed:', err);
+      throw err;
+    });
+};
+
+export const get = (endpoint) => apiCall('GET', endpoint);
+export const post = (endpoint, data) => apiCall('POST', endpoint, data);
+export const put = (endpoint, data) => apiCall('PUT', endpoint, data);
+export const remove = (endpoint) => apiCall('DELETE', endpoint);
 
